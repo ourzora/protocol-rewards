0ef70e6c4e9e5e6426774e79a141cdf078e0934d
kulkarohan <kulkarohan@gmail.com>
2023-06-01 00:48:38 -0400

    chore: initial commit

---
+++ src/ZoraRewards.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import { IZoraRewards } from "./interfaces/IZoraRewards.sol";
+
+contract ZoraRewards is IZoraRewards {
+    constructor() payable { }
+}
---
+++ test/ZoraRewards.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import "../src/ZoraRewards.sol";
+
+contract ZoraRewardsTest is Test {
+    function setUp() public { }
+}
