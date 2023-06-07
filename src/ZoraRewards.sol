daa9c9f7b84e52ea667efd5b9b80ca344ddab39b
kulkarohan <kulkarohan@gmail.com>
2023-06-06 22:21:23 -0400

    chore: update OZ path

--- src/ZoraRewards.sol
+++ src/ZoraRewards.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import { ERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import { IZoraRewards } from "./interfaces/IZoraRewards.sol";
 
