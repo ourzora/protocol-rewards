8f86d8ec1b86e41261b901a3f5c85f090f4bfa3b
kulkarohan <kulkarohan@gmail.com>
2023-06-22 10:10:50 -0400

    chore: add test nfts

---
+++ test/utils/MockNFTs.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+import { ERC721Rewards } from "../../src/ERC721/ERC721Rewards.sol";
+import { ERC1155Rewards } from "../../src/ERC1155/ERC1155Rewards.sol";
+
+contract MockERC721 is ERC721, ERC721Rewards {
+    uint256 public salePrice;
+    uint256 public currentTokenId;
+
+    constructor(address _creator, address _zoraRewards, address _zoraRewardRecipient)
+        ERC721("Mock ERC721", "MOCK")
+        ERC721Rewards(_zoraRewards, _zoraRewardRecipient)
+    { }
+
+    function setSalePrice(uint256 _salePrice) external {
+        salePrice = _salePrice;
+    }
+
+    function mint(address to, uint256 numTokens, address creator, address finder, address lister) external payable {
+        _handleRewards(msg.value, numTokens, salePrice, creator, finder, lister);
+
+        for (uint256 i; i < numTokens; ++i) {
+            _mint(to, currentTokenId++);
+        }
+    }
+}
+
+contract MockERC1155 is ERC1155, ERC1155Rewards {
+    uint256 public salePrice;
+
+    constructor(address _creator, address _zoraRewards, address _zoraRewardRecipient)
+        ERC1155("Mock ERC1155 URI")
+        ERC1155Rewards(_zoraRewards, _zoraRewardRecipient)
+    { }
+
+    function setSalePrice(uint256 _salePrice) external {
+        salePrice = _salePrice;
+    }
+
+    function mint(address to, uint256 tokenId, uint256 numTokens, address creator, address finder, address lister)
+        external
+        payable
+    {
+        uint256 remainingValue = _handleRewardsAndGetValueSent(msg.value, numTokens, creator, finder, lister);
+
+        require(remainingValue == salePrice, "MockERC1155: incorrect value");
+
+        _mint(to, tokenId, numTokens, "");
+    }
+}
