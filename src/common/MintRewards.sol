9e903a1974bceaeea2dcde0255460cbdf237b505
Rohan Kulkarni <kulkarohan@gmail.com>
2023-06-27 17:22:03 -0400

    refactor: update computation helpers

--- src/common/MintRewards.sol
+++ src/common/MintRewards.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import { IMintRewards } from "../interfaces/IMintRewards.sol";
 import { IZoraRewards } from "../interfaces/IZoraRewards.sol";
 
-abstract contract MintRewards is IMintRewards {
-    bytes4 public constant ZORA_FREE_MINT_REWARD_TYPE = bytes4(keccak256("ZORA_FREE_MINT_REWARD"));
-    bytes4 public constant ZORA_PAID_MINT_REWARD_TYPE = bytes4(keccak256("ZORA_PAID_MINT_REWARD"));
-
+abstract contract MintRewards {
     uint256 internal constant TOTAL_REWARD_PER_MINT = 0.000999 ether;
 
     uint256 internal constant CREATOR_REWARD_FREE_MINT = 0.000555 ether;
@@ -22,6 +18,9 @@ abstract contract MintRewards is IMintRewards {
     address internal immutable ZORA_REWARD_RECIPIENT;
     IZoraRewards internal immutable ZORA_REWARDS;
 
+    error INVALID_ADDRESS_ZERO();
+    error INSUFFICIENT_ETH_FOR_REWARDS();
+
     constructor(address _zoraRewards, address _zoraRewardRecipient) payable {
         if (_zoraRewards == address(0) || _zoraRewardRecipient == address(0)) {
             revert INVALID_ADDRESS_ZERO();
@@ -35,38 +34,32 @@ abstract contract MintRewards is IMintRewards {
         return numTokens * TOTAL_REWARD_PER_MINT;
     }
 
-    function computeTotalValue(uint256 numTokens, uint256 salePrice) public pure returns (uint256) {
-        uint256 totalReward = computeTotalReward(numTokens);
-        uint256 totalSale = numTokens * salePrice;
-        return totalReward + totalSale;
-    }
-
     function computeFreeMintRewards(uint256 numTokens)
         public
         pure
         returns (
             uint256 totalReward,
             uint256 creatorReward,
-            uint256 zoraReward,
             uint256 finderReward,
-            uint256 listerReward
+            uint256 listerReward,
+            uint256 zoraReward
         )
     {
         totalReward = computeTotalReward(numTokens);
         creatorReward = numTokens * CREATOR_REWARD_FREE_MINT;
-        zoraReward = numTokens * ZORA_REWARD_FREE_MINT;
         finderReward = numTokens * FINDER_REWARD_FREE_MINT;
         listerReward = numTokens * LISTER_REWARD_FREE_MINT;
+        zoraReward = numTokens * ZORA_REWARD_FREE_MINT;
     }
 
     function computePaidMintRewards(uint256 numTokens)
         public
         pure
-        returns (uint256 totalReward, uint256 zoraReward, uint256 finderReward, uint256 listerReward)
+        returns (uint256 totalReward, uint256 finderReward, uint256 listerReward, uint256 zoraReward)
     {
         totalReward = computeTotalReward(numTokens);
-        zoraReward = numTokens * ZORA_REWARD_PAID_MINT;
         finderReward = numTokens * FINDER_REWARD_PAID_MINT;
         listerReward = numTokens * LISTER_REWARD_PAID_MINT;
+        zoraReward = numTokens * ZORA_REWARD_PAID_MINT;
     }
 }
