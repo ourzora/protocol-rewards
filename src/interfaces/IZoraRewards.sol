9da6d9df99db40dcdc6a1592d6566d69d640bb3a
Rohan Kulkarni <kulkarohan@gmail.com>
2023-07-13 16:25:37 -0400

    refactor: update zora rewards interface

--- src/interfaces/IZoraRewards.sol
+++ src/interfaces/IZoraRewards.sol
@@ -2,48 +2,25 @@
 pragma solidity 0.8.17;
 
 interface IZoraRewards {
-    event ZoraRewardsMint(
-        bytes4 indexed rewardType,
+    event Deposit(address indexed from, address indexed recipient, uint256 indexed amount);
+    event DepositCreatorRewards(
         address from,
         address indexed creator,
         uint256 creatorReward,
         address indexed mintReferral,
         uint256 mintReferralReward,
-        address createReferral,
+        address indexed createReferral,
         uint256 createReferralReward,
         address zora,
         uint256 zoraReward
     );
-    event ZoraRewardsMint(address indexed from, address indexed recipient, uint256 indexed reward, string comment);
-    event ZoraRewardsBurn(address indexed from, address indexed recipient, uint256 amount);
+    event Withdraw(address indexed owner, uint256 indexed amount);
 
+    error ADDRESS_ZERO();
+    error ARRAY_LENGTH_MISMATCH();
     error INVALID_DEPOSIT();
+    error INVALID_SIGNATURE();
     error INVALID_WITHDRAW();
-    error RECIPIENTS_AND_AMOUNTS_LENGTH_MISMATCH();
-    error TRANSFER_FAILED();
     error SIGNATURE_DEADLINE_EXPIRED();
-    error INVALID_SIGNER();
-
-    function deposit(address recipient, string calldata comment) external payable;
-    function depositBatch(address[] calldata recipients, uint256[] calldata rewards, string calldata comment)
-        external
-        payable;
-    function depositFreeMintRewards(
-        address creator,
-        uint256 creatorReward,
-        address mintReferral,
-        uint256 mintReferralReward,
-        address createReferral,
-        uint256 createReferralReward,
-        address zora,
-        uint256 zoraReward
-    ) external payable;
-    function depositPaidMintRewards(
-        address mintReferral,
-        uint256 mintReferralReward,
-        address createReferral,
-        uint256 createReferralReward,
-        address zora,
-        uint256 zoraReward
-    ) external payable;
+    error TRANSFER_FAILED();
 }
