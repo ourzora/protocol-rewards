6aafd601ca65d9aee04bade981cf241395ae3543
Rohan Kulkarni <kulkarohan@gmail.com>
2023-07-28 09:27:47 -0600

    refactor: rename mint rewards to protocol rewards

--- src/abstract/ProtocolRewards.sol
+++ src/abstract/ProtocolRewards.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.17;
 
 import { IZoraRewards } from "../interfaces/IZoraRewards.sol";
 
-abstract contract MintRewards {
+abstract contract ProtocolRewards {
     error CREATOR_FUNDS_RECIPIENT_NOT_SET();
     error INVALID_ADDRESS_ZERO();
     error INVALID_ETH_AMOUNT();
